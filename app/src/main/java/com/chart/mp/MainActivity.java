bc40fd501dc6ea7966e632a6c5f27d666656d74f
RuhulRuhul <ruhulamin122017@gmail.com>
2021-05-07 20:28:44 +0530

    Initial commit

---
+++ app/src/main/java/com/chart/mp/MainActivity.java
@@ -0,0 +1,48 @@
+package com.chart.mp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnLineChart,btnBarChart,btnPieChart;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnLineChart = findViewById(R.id.btnLineChart);
+        btnBarChart = findViewById(R.id.btnBarChart);
+        btnPieChart = findViewById(R.id.btnPieChart);
+        btnLineChart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,LineChartActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        btnBarChart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,BarChartActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        btnPieChart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,PieChartActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
